aa618c40faf4f8a147970e99d16df556137faa90
Alex Borges <62350622+alex-breis@users.noreply.github.com>
2022-08-04 23:06:43 -0300

    Aula 1

---
+++ TestaValor.py
@@ -0,0 +1,9 @@
+import math
+
+val = input("Entre com um número: ")
+val = int(val)
+
+if pow(val, 2) <= 100:
+    print("Menor que 100")
+else: 
+    print("Maior que 100")
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+
+print("Hello World")
+
+if __name__ == "__main__":
+    curso = "Introdução ao Python"
+    
